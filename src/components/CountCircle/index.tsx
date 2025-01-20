dc463a3bc1bc60fe1a82763681e6c9fbe553db91
linying-tastien <2227580291@qq.com>
2025-01-20 19:34:41 +0800

    chore: tailwind

--- src/components/CountCircle/index.tsx
+++ src/components/CountCircle/index.tsx
@@ -1,27 +1,32 @@
 import useCountDown from "@/hooks/useCountDown";
 import React, { useEffect } from "react";
 import "./index.scss";
+import { View } from "@tarojs/components";
 
 const CountCircle: React.FC<{ duration: number }> = ({ duration }) => {
   const { countdown: timeLeft, start } = useCountDown({ duration });
 
   useEffect(() => {
     start(); // 启动倒计时
-  }, [start]);
+  }, []);
 
   const percentage = (timeLeft / duration) * 100;
 
+  const hours = Math.floor((timeLeft / 1000 / 3600) % 24);
+  const minutes = Math.floor((timeLeft / 1000 / 60) % 60);
+  const seconds = Math.floor((timeLeft / 1000) % 60);
+
   return (
-    <div className="count-circle">
-      <div
+    <View className="count-circle mt-4">
+      <View
         className="circle"
         style={{
           background: `conic-gradient(pink ${percentage}%, lightgray ${percentage}%)`,
         }}
       >
-        <div className="timer">{timeLeft}s</div>
-      </div>
-    </div>
+        <View className="timer">{`${hours}:${minutes}:${seconds}`}</View>
+      </View>
+    </View>
   );
 };
 
--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,8 +1,8 @@
+import CountCircle from "@/components/CountCircle";
 import dayjs from "dayjs";
 import { View, Text } from "@tarojs/components";
 import { useLoad } from "@tarojs/taro";
 import "./index.scss";
-import CountCircle from "@/components/CountCircle";
 
 export default function Index() {
   useLoad(() => {
@@ -10,9 +10,8 @@ export default function Index() {
   });
 
   return (
-    <View className="index">
-      <CountCircle duration={dayjs(8, "hours").valueOf()} />
-      <Text>Hello world!</Text>
+    <View>
+      <CountCircle duration={16 * 60 * 60 * 1000} />
     </View>
   );
 }
